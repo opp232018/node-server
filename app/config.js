eaad494b09b0a5405945edd72f203e99dd0f0e0e
opp232018 <pioquinto.oliver@gmail.com>
2018-09-23 13:21:51 +0800

    Installed Nodemon

--- app/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Config = {
-    MONGO_CON_URL: process.env.MONGO_CON_URL
-};
--- app/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Routes = /** @class */ (function () {
-    function Routes(serverApp) {
-    }
-    return Routes;
-}());
-exports.default = Routes;
